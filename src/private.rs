653f3dfcee9e4cf7d095ac6ff67817f27cd6f77b
Paweł Jastrzębski <paweljastrzebski96@gmail.com>
2023-07-13 10:09:45 +0200

    fix tokio block runtime

--- src/private.rs
+++ src/private.rs
@@ -1,10 +1,10 @@
 use crate::{Callback, Lazy, Service};
 use std::future::Future;
 
-#[cfg(feature = "tokio")]
-use tokio::task::spawn_blocking as spawn_blocking;
 #[cfg(not(feature = "tokio"))]
 use std::thread::spawn as spawn_blocking;
+#[cfg(feature = "tokio")]
+use tokio::task::spawn_blocking;
 
 #[inline]
 #[doc(hidden)]
@@ -21,22 +21,43 @@ pub const fn lazy_construct<T>(value: fn() -> T) -> Lazy<T> {
 #[inline]
 #[doc(hidden)]
 pub fn block_callback<T, F, O>(future: fn(Callback<T>) -> F) -> T
-    where T: Send + 'static, F: Future<Output=O> + 'static,
+where
+    T: Send + 'static,
+    F: Future<Output = O> + 'static,
 {
     let (tx, rx) = std::sync::mpsc::sync_channel(1);
     let callback = Callback::new(tx);
-    spawn_blocking(move || { block(future(callback)); });
+    spawn_blocking(move || {
+        block(future(callback));
+    });
     rx.recv().expect("\nCallback<T>.call(T) was not called\n")
 }
 
+#[doc(hidden)]
+#[cfg(feature = "tokio")]
+static BLOCK_RUNTIME: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .worker_threads(2)
+        .build()
+        .expect("tokio test runtime")
+});
+
 #[inline]
 #[doc(hidden)]
-pub fn block<T>(future: impl Future<Output=T>) -> T {
-    futures_lite::future::block_on(future)
+pub fn block<T>(future: impl Future<Output = T>) -> T {
+    #[cfg(feature = "tokio")]
+    {
+        tokio::task::block_in_place(move || BLOCK_RUNTIME.block_on(future))
+    }
+    #[cfg(not(feature = "tokio"))]
+    {
+        futures_lite::future::block_on(future)
+    }
 }
 
 #[inline]
 #[doc(hidden)]
-pub fn block_fn<D, F: Future<Output=D>>(future: fn() -> F) -> D {
+pub fn block_fn<D, F: Future<Output = D>>(future: fn() -> F) -> D {
     block(future())
 }
